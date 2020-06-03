00f2fb8e1b9433de951faee5596324cd4d52fee8
Michael O'Driscoll <MODriscoll55@outlook.com>
2020-06-03 21:25:05 +1000

    Adding Pause Menu script and HUD. Also adding a script for managing the HUDs the player can have (right now Game HUD and Pause Menu)

---
+++ In The Dark/Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private UserPrompt m_quitPrompt = null;        // User prompt to display when attempting to quit
+
+    /// <summary>
+    /// Resumes the current game session
+    /// </summary>
+    public void ResumeSession()
+    {
+        if (PlayerHUD.instance)
+            // TODO: Would ideally have a GameManager.Unpause()
+            PlayerHUD.instance.HidePauseScreen();
+    }
+
+    /// <summary>
+    /// Quits the current game session, but will first display user prompt to confirm
+    /// </summary>
+    public void QuitSession()
+    {
+        if (!UserPrompt.DisplayPrompt(m_quitPrompt, OnQuitSessionConfirmed))
+            SceneManager.LoadScene(0);  // Assuming Main Menu is scene 0
+    }
+
+    /// <summary>
+    /// Notify that user has made a selection on the prompt when attempting to quit
+    /// </summary>
+    /// <param name="result"></param>
+    private void OnQuitSessionConfirmed(UserPrompt.UserPromptResult result)
+    {
+        if (result == UserPrompt.UserPromptResult.Ok)
+            SceneManager.LoadScene(0);  // Assuming Main Menu is scene 0
+    }
+}
---
+++ In The Dark/Assets/Scripts/Player/PlayerHUD.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script handles the players HUD. This doesn't mean the Canvas
+/// directly, but any UI widget that could be displayed
+/// </summary>
+public class PlayerHUD : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton access to players HUD, may not be valid
+    /// </summary>
+    public static PlayerHUD instance { get; private set; }
+
+    [SerializeField] private GameObject m_gameHUD = null;               // Object with core Game HUD
+    [SerializeField] private PauseMenu m_pauseMenuPrefab = null;        // Prefab for pause menu to spawn
+
+    private PauseMenu m_pauseMenu = null;       // Reference to instance of Pause Menu
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Duplicate PlayerHUD instantiated. This instance will not be used as singleton instance", this);
+            return;
+        }
+
+        instance = this;
+    }
+
+    void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
+
+    void Update()
+    {
+        // Only update if primary instance
+        if (this != instance)
+            return;
+
+        // TODO: First, make an input binding
+        // Second, we should handle this in a central GameManager?
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (m_pauseMenu)
+                HidePauseScreen();
+            else
+                DisplayPauseScreen();
+        }
+    }
+
+    public void DisplayPauseScreen()
+    {
+        if (m_pauseMenu || !m_pauseMenuPrefab)
+            return;
+
+        SetGameHUDVisible(false);
+        m_pauseMenu = Instantiate(m_pauseMenuPrefab);
+
+        // TODO: we should handle this in a central GameManager?
+        // Scripts could then use GameManager.isPaused to check if they should update
+        // We basically are 'Paused' if the menu was created (though the game should be pausable even without the HUD)
+        if (m_pauseMenu)
+            Time.timeScale = 0f;
+    }
+
+    public void HidePauseScreen()
+    {
+        if (!m_pauseMenu)
+            return;
+
+        Destroy(m_pauseMenu.gameObject);
+        m_pauseMenu = null;
+
+        SetGameHUDVisible(true);
+
+        // TODO: we should handle this in a central GameManager?
+        // Scripts could then use GameManager.isPaused to check if they should update
+        Time.timeScale = 1f;
+    }
+
+    private void SetGameHUDVisible(bool visible)
+    {
+        if (m_gameHUD)
+            m_gameHUD.SetActive(visible);
+    }
+}
