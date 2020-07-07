0fcbd982a90a48fed304c916c05b009ed9ae3dc3
James <j.bishara@hotmail.com>
2020-07-07 15:18:18 +1000

    Started making prototype scene for week 4

---
+++ In The Dark/Assets/Scripts/Dialogue/JB_Dialogue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class JB_Dialogue : ScriptableObject
+{
+    public dialogueData[] interactions;
+}
+
+
+[System.Serializable]
+public struct dialogueData
+{
+    public Sprite characterSprite;
+    public string name;
+    [TextArea(3, 10)]
+    public string sentences;
+}
+
