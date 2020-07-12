d7022f7ee8aeb83570260764394522e99321aa7b
Michael O'Driscoll <MODriscoll55@outlook.com>
2020-07-12 22:37:57 +1000

    Only levels that can be unlocked are selectable in the Level Select screen

--- In The Dark/Assets/Scripts/Menus/LevelSelectMenu.cs
+++ In The Dark/Assets/Scripts/Menus/LevelSelectMenu.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelectMenu : MonoBehaviour
 {
-    [SerializeField] private List<string> m_levelNames = new List<string>();        // List of all the levels that can be played
+    [System.Serializable]
+    class LevelData
+    {
+        public string m_levelName = string.Empty;
+        public Button m_levelButton = null;
+
+        public void UpdateButtonStatus(NinjasSaveData saveData)
+        {
+            if (!m_levelButton)
+                return;
+
+            bool unlockedLevel = saveData.m_unlockedLevels.Contains(m_levelName);
+            m_levelButton.interactable = unlockedLevel;
+        }
+    }
+
+    [SerializeField] private List<LevelData> m_levelsData = new List<LevelData>();          // List of all the levels that can be played
 
     private NinjasSaveData m_saveData = null;       // Loaded save data
 
@@ -14,7 +31,18 @@ public class LevelSelectMenu : MonoBehaviour
 
         // No save data exist
         if (m_saveData == null)
+        {
+            // Create new save data, save first level as current and unlocked
             m_saveData = new NinjasSaveData();
+
+            if (m_levelsData.Count > 0)
+                m_saveData.m_unlockedLevels.Add(m_levelsData[0].m_levelName);
+
+            m_saveData.Save();
+        }
+
+        for (int i = 0; i < m_levelsData.Count; ++i)
+            m_levelsData[i].UpdateButtonStatus(m_saveData);
     }
 
     void OnDisable()
@@ -31,8 +59,8 @@ public class LevelSelectMenu : MonoBehaviour
         string levelName = m_saveData.m_currentLevel;
         if (levelName == string.Empty)
         {
-            if (m_levelNames.Count > 0)
-                levelName = m_levelNames[0];
+            if (m_levelsData.Count > 0)
+                levelName = m_levelsData[0].m_levelName;
         }
 
         if (GameManager.OpenLevel(levelName))
@@ -44,12 +72,12 @@ public class LevelSelectMenu : MonoBehaviour
 
     public void SelectLevel(int levelIndex)
     {
-        if (levelIndex >= 0 && levelIndex < m_levelNames.Count)
+        if (levelIndex >= 0 && levelIndex < m_levelsData.Count)
         {
-            string levelName = m_levelNames[levelIndex];
-            if (GameManager.OpenLevel(levelName))
+            LevelData data = m_levelsData[levelIndex];
+            if (GameManager.OpenLevel(data.m_levelName))
             {
-                m_saveData.m_currentLevel = levelName;
+                m_saveData.m_currentLevel = data.m_levelName;
                 m_saveData.Save();
             }
         }
