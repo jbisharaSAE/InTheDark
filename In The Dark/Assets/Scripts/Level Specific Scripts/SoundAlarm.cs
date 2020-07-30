2f220a45f096b1f726c2a8b58b47c17bc259c59c
conquis <mylesokorn@hotmail.com>
2020-07-30 18:55:03 +1000

    Town Level & Level scripts
    - playtest ready town level, missing set dressing
    - ambush script working, missing door open after completion
    - alarm horn script working, missing sprite for horn
    - needs player added to level
    -needs wall jumping to be tested for distance

---
+++ In The Dark/Assets/Scripts/Level Specific Scripts/SoundAlarm.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAlarm : MonoBehaviour
+{
+    public GameObject routeHolder;
+    public GameObject[] routes;
+    public GameObject player;
+    public bool trigger;
+    public bool test;
+
+    private void Start()
+    {
+        trigger = false;
+        player = GameObject.FindWithTag("Player"); 
+    }
+
+    private void Update(){
+        if (test)
+        {
+            test = false;
+            Alarm();
+        }
+    }
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player") && !trigger)
+        {
+            trigger = true;
+            Alarm();
+        }
+    }
+
+   public void Alarm()
+   {
+        foreach(GameObject i in routes)
+        {
+            i.GetComponent<Transform>().SetPositionAndRotation(routeHolder.transform.position, this.transform.rotation);
+        }
+   }
+}
---
+++ In The Dark/Assets/Scripts/Level Specific Scripts/TownAmbush.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownAmbush : MonoBehaviour
+{
+    public GameObject[] badGuys;
+    public GameObject player;
+    public bool trigger;
+    public bool test;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (test)
+        {
+            test = false;
+            Ambush();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player") && !trigger)
+        {
+            trigger = true;
+            Ambush();
+        }
+    }
+
+    public void Ambush()
+    {
+        foreach (GameObject i in badGuys)
+        {
+            i.SetActive(true);
+        }
+    }
+
+    // need to add trigger for beating both bad guys to open door to leave room
+}
