e38a7b82d8aaf52c4a6e8a2bb82a128c8bc13ec7
Michael O'Driscoll <MODriscoll55@outlook.com>
2020-08-04 22:43:48 +1000

    Addings sounds for healing/taking damage

---
+++ In The Dark/Assets/Scripts/Gameplay/CharacterSounds.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSounds : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] private HealthComponent m_healthComp = null;
+    [SerializeField] private AudioSource m_healthChangeAudioSource = null;
+    [SerializeField] private AudioClip m_healAudio = null;
+    [SerializeField] private AudioClip m_hurtAudio = null;
+    [SerializeField, Range(0f, 1f)] private float m_hurtPitchRange = 0.2f;
+
+    void Awake()
+    {
+        if (m_healthComp)
+            m_healthComp.OnHealthChanged += OnHealthChanged;
+    }
+
+    void OnDestroy()
+    {
+        if (m_healthComp)
+            m_healthComp.OnHealthChanged -= OnHealthChanged;
+    }
+
+    void OnHealthChanged(HealthComponent self, float newHealth, float delta)
+    {
+        if (self.isDead)
+            return;
+
+        // Positive delta = heal, Negative delta = damage
+        if (delta > 0f)
+            PlayHealthChangeAudio(m_healAudio, 0f);
+        else
+            PlayHealthChangeAudio(m_hurtAudio, m_hurtPitchRange);
+    }
+
+    private void PlayHealthChangeAudio(AudioClip clip, float pitchRange)
+    {
+        if (!m_healthChangeAudioSource || !clip)
+            return;
+
+        m_healthChangeAudioSource.Stop();
+
+        m_healthChangeAudioSource.clip = clip;
+        m_healthChangeAudioSource.loop = false;
+
+        m_healthChangeAudioSource.pitch = 1f;
+        if (pitchRange > 0f)
+            m_healthChangeAudioSource.pitch += Random.Range(-pitchRange, pitchRange);
+
+        m_healthChangeAudioSource.Play();      
+    }
+}
---
+++ In The Dark/Assets/Scripts/Gameplay/SingleSound.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SingleSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_audioSource = null;
+
+    public AudioClip clip { set { if (m_audioSource) m_audioSource.clip = value; } }
+
+    void Awake()
+    {
+        if (!m_audioSource)
+            m_audioSource = GetComponent<AudioSource>();
+
+        if (!m_audioSource)
+            m_audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        PlaySingleTime();
+    }
+
+    private void PlaySingleTime()
+    {
+        if (!m_audioSource.clip)
+            return;
+
+        Invoke("FinishedPlayingAudio", m_audioSource.clip.length);
+    }
+
+    private void FinishedPlayingAudio()
+    {
+        Destroy(gameObject);
+    }
+
+    public static void PlaySingleSound(AudioClip clip)
+    {
+        if (!clip)
+            return;
+
+        SingleSound singleSound = new GameObject().AddComponent<SingleSound>();
+        singleSound.clip = clip;
+
+        singleSound.PlaySingleTime();
+    }
+}
