0357b4c58ced8537bbc6b35e4650a55ba54024b8
James <j.bishara@hotmail.com>
2020-09-01 17:16:52 +1000

    added small force to health pickup items

--- In The Dark/Assets/Scripts/Objects/Pickups/JB_HealthPickup.cs
+++ In The Dark/Assets/Scripts/Objects/Pickups/JB_HealthPickup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class JB_HealthPickup : MonoBehaviour
 {
     private Rigidbody2D m_RigidBody;
+    public float speed;
 
     [SerializeField] private float power;
 
@@ -13,13 +14,19 @@ public class JB_HealthPickup : MonoBehaviour
     {
         m_RigidBody = GetComponent<Rigidbody2D>();
 
-        float randX = Random.Range(-10f, 10f);
-        float randY = Random.Range(-10f, 10f);
+        float randX = Random.Range(-5f, 5f);
+        float randY = Random.Range(-5f, 5f);
 
-        Vector2 dir = new Vector2(randX, randY);
-        dir.Normalize();
+        //Vector2 dir = new Vector2(randX, randY);
+
+        Vector2 dir = Random.insideUnitSphere.normalized;
+        //dir.Normalize();
         m_RigidBody.AddForce((dir * power));
     }
 
-    
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward *speed * Time.deltaTime);
+    }
+
 }
